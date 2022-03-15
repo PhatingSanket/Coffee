2f629351fd0a6eb808125adb11645274cc45332b
PhatingSanket <86225220+PhatingSanket@users.noreply.github.com>
2022-03-15 09:35:55 +0100

    Add project files.

---
+++ src/Foundation/GlassMapper/code/Models/IGlassBase.cs
@@ -0,0 +1,32 @@
+﻿using Glass.Mapper.Sc.Configuration;
+using Glass.Mapper.Sc.Configuration.Attributes;
+using Sitecore.Data.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Coffee.Foundation.GlassMapper.Models
+{
+    [SitecoreType(AutoMap = true)]
+    public interface IGlassBase
+    {
+        [SitecoreId]
+        Guid Id { get; }
+
+        [SitecoreInfo(SitecoreInfoType.Name)]
+        string Name { get; set; }
+
+        [SitecoreInfo(SitecoreInfoType.TemplateId)]
+        Guid TemplateId { get; set; }
+
+        [SitecoreItem]
+        Item Item { get; }
+
+        [SitecoreInfo(SitecoreInfoType.Url)]
+        string Url { get; }
+
+        [SitecoreInfo(SitecoreInfoType.DisplayName)]
+        string DisplayName { get; }
+    }
+}
